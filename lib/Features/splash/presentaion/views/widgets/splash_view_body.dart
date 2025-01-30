aee1aa44b781c4e2fdc826ed806ce61d71770f02
MariamEssam <romaessam570@gmail>
2025-01-30 16:42:24 +0200

    change theme & add text to splash view

--- lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentaion/views/widgets/splash_view_body.dart
@@ -9,7 +9,17 @@ class SplashViewBody extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [Image.asset(AssetsData.logo)],
+      children: [
+        Image.asset(AssetsData.logo),
+        const SizedBox(
+          height: 5,
+        ),
+        const Text(
+          "Reed Free Books",
+          style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
