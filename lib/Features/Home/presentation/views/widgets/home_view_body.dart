006dbd6789b7779fe0b64801da5d3bb916cb5d3c
MariamEssam <romaessam570@gmail>
2025-02-07 01:39:48 +0200

    create styles file and add title to home view

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:booklyapp/Core/utils/assets.dart';
+import 'package:booklyapp/Core/utils/styles.dart';
 import 'package:booklyapp/Features/Home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:booklyapp/Features/Home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:booklyapp/Features/Home/presentation/views/widgets/featured_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -9,30 +11,25 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBoxListView(),
-      ],
-    );
-  }
-}
-
-class FeaturedBoxListView extends StatelessWidget {
-  const FeaturedBoxListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .23,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: FeaturedListViewItem(),
-            );
-          }),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBoxListView(),
+          SizedBox(
+            height: 45,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Best Seller ",
+              style: Styles.titleMedium,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
